705b35ab3bfb57bbb7ebedc30b5c9fac0ab4dd05
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-03 18:23:11 +0700

    Only create custom rc files if not exist

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ test -f ~/.bashrc && rm ~/.bashrc
 test -f ~/.bash_profile && rm ~/.bash_profile
 test -f ~/.gitconfig && rm ~/.gitconfig
 test -f ~/.vimrc && rm ~/.vimrc
-test -f ~/.bash_aliases && rm ~/.bash_aliases
-test -f ~/.custom.bashrc && rm ~/.custom.bashrc
 
 echo "Copying dotfiles..."
 ln -s ~/.dotfiles/common/.bash_profile ~/.bash_profile
@@ -13,6 +11,6 @@ ln -s ~/.dotfiles/common/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/common/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/common/.vimrc ~/.vimrc
 
-echo "Creating custom .bash_aliases and .custom.bashrc..."
-touch ~/.bash_aliases
-touch ~/.custom.bashrc
+echo "Creating .bash_aliases and .custom.bashrc (if not exists)..."
+test -f ~/.bash_aliases || touch ~/.bash_aliases
+test -f ~/.custom.bashrc || touch ~/.custom.bashrc
