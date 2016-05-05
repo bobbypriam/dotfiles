7f8f948e76e2820e72bf22a9ff07fbe0a2237d75
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-05-05 16:31:09 +0700

    Add vundle-plugins to vim

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ echo "Copying dotfiles..."
 ln -sf ~/.dotfiles/common/bash/bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/common/bash/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/common/git/gitconfig ~/.gitconfig
+
+mkdir -p ~/.vim
 ln -sf ~/.dotfiles/common/vim/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/common/vim/vundle-plugins ~/.vim/vundle-plugins
 
 echo "Creating .bash_aliases and .custom.bashrc (if not exists)..."
 test -f ~/.bash_aliases || touch ~/.bash_aliases
