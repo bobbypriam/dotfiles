0d5507c358f8bd61b1e47b35947d11a4fa9d03dd
Bobby Priambodo <bobby.priambodo@gmail.com>
2016-05-05 16:27:44 +0700

    Remove leading dots

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ test -f ~/.gitconfig && rm ~/.gitconfig
 test -f ~/.vimrc && rm ~/.vimrc
 
 echo "Copying dotfiles..."
-ln -sf ~/.dotfiles/common/bash/.bash_profile ~/.bash_profile
-ln -sf ~/.dotfiles/common/bash/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/common/git/.gitconfig ~/.gitconfig
-ln -sf ~/.dotfiles/common/vim/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/common/bash/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/common/bash/bashrc ~/.bashrc
+ln -sf ~/.dotfiles/common/git/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/common/vim/vimrc ~/.vimrc
 
 echo "Creating .bash_aliases and .custom.bashrc (if not exists)..."
 test -f ~/.bash_aliases || touch ~/.bash_aliases
