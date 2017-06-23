e75a0f967fc121f2beed1e29ba470913b1f67ce6
Bobby Priambodo <bobby.priambodo@gmail.com>
2017-06-23 14:32:30 +0700

    Update osx installation scripts

--- osx/install-dependencies.sh
+++ osx/install-dependencies.sh
@@ -1,25 +1,27 @@
 #/bin/sh
 
-brew install curl wget axel
-brew install vim
-brew install zsh
-brew install git git-extras
-brew install htop tree nmap lynx openssl nginx pv
-brew install docker docker-compose docker-machine
-brew install python python3
-brew install imagemagick
-brew install brew-rmtree
+# SHELL PROGRAMS
 
-brew cask install google-ehrome
-brew cask install atom
-brew cask install firefox
-brew cask install iterm2
-brew cask install spotify
-brew cask install torbrowser
-brew cask install virtualbox
-brew cask install vlc
-brew cask install xrg
-brew cask install mysqlworkbench
-brew cask install the-unarchiver
-brew cask install flux
+brew install \
+  curl wget axel coreutils \
+  zsh \
+  neovim emacs \
+  git git-extras \
+  htop tree nmap lynx openssl nginx pv \
+  imagemagick \
+  python python3 \
+  brew-rmtree \
+  ocaml opam \
+  elixir \
+  ruby
 
+# GUI PROGRAMS
+
+brew cask install \
+  google-chrome firefox \
+  atom macvim visual-studio-code sublime-text \
+  spotify vlc google-drive \
+  iterm2 \
+  docker virtualbox mysqlworkbench \
+  font-hasklig font-fira-code font-source-code-pro font-hack \
+  xrg the-unarchiver flux karabiner seil itsycal
